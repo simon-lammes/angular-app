7daab2f78efe5a0b9ad455cd7103778a025c367e
simon-lammes <simon.lammes@cofinpro.de>
2019-02-27 21:37:26 +0100

    difficultyDescription

--- src/app/minesweeper/difficulty.ts
+++ src/app/minesweeper/difficulty.ts
@@ -3,6 +3,9 @@ export class Difficulty {
   rowCount: number;
   columnCount: number;
   bombCount: number;
+  get description(): string {
+    return `${this.name} ||| ${this.rowCount}X${this.columnCount} ||| ${this.bombCount} bombs`;
+  }
 
   constructor(name: string, rowCount: number, columnCount: number, bombCount: number) {
     this.name = name;
